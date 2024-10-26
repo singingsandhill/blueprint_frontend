f1c7dc88bcd6c634501411c1a9f027926474d69d
kim jiwoo <annejwkim00@naver.com>
2024-10-26 21:49:49 +0900

    Fix: 아이콘 라이브러리 방식으로 변경
    기존 kit방식에서 라이브러리 방식으로 변경

--- src/config/index.js
+++ src/config/index.js
@@ -1,35 +1,33 @@
 export default {
-  title: "청사진", // 메인 타이틀
-  subtitle: "정책 추천 서비스", // 서브 타이틀
+  title: "청사진",
+  subtitle: "정책 추천 서비스",
   menus: [
-    // 메인 메뉴 구성 정보
     {
       title: "정책",
       url: "/policy",
-      icon: "fa-solid fa-paste",
+      icon: ["fas", "paste"],
     },
     {
       title: "청약",
       url: "/subscription",
-      icon: "fas fa-building",
+      icon: ["fas", "building"],
     },
     {
       title: "금융",
       url: "/economy",
-      icon: "fa-solid fa-money-bill-1",
+      icon: ["fas", "money-bill"],
     },
   ],
   accountMenus: {
-    // 인증 관련 메뉴 정보
     login: {
       url: "/auth/login",
       title: "로그인",
-      icon: "far fa-user",
+      icon: ["far", "user"],
     },
     join: {
       url: "/auth/join",
       title: "회원가입",
-      icon: "fa-solid fa-user-plus",
+      icon: ["fas", "user-plus"],
     },
   },
 };
