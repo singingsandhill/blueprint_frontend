de583a34ff69ce24553530c4d14e8b5bca397d04
jisu <jisu0259@gmail.com>
2024-11-06 20:50:42 +0900

    Feat: 청약 정보  UI 구현

--- src/stores/subscription.js
+++ src/stores/subscription.js
@@ -1,18 +1,18 @@
 import { defineStore } from "pinia";
 import axios from "axios";
 
-export const useSubsciptionStore = defineStore("subscription",{
+export const useSubscriptionStore = defineStore("subscription",{
     state:()=>({
-        subscription: []
+        subscription: [],
     }),
 
 actions: {
     async getSubscription() {
         try{
-            const response = await axios.get("http://localhost:8080:/subscription/get")
+            const response = await axios.get("http://localhost:8080/subscription/get")
             this.subscription = response.data.response.data;
         } catch (error) {
-            console.log(error);
+            console.log("Error fetching subscription data:", error);
         }
     }
 }
