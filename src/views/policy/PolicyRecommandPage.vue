a23df9fa36b2f08549a72bdde9421a0fbacc074e
kim jiwoo <annejwkim00@naver.com>
2024-11-10 20:06:32 +0900

    Fix: 정책 UI 변경

--- src/views/policy/PolicyRecommandPage.vue
+++ src/views/policy/PolicyRecommandPage.vue
@@ -1,6 +1,6 @@
-<script setup>
+<!-- <script setup>
 import { usePolicyStore } from "@/stores/policy.js";
-import { ref, onMounted } from "vue";
+import { ref } from "vue";
 
 const policyStore = usePolicyStore();
 const policyList = ref([]);
@@ -81,9 +81,9 @@ const applyFilters = async () => {
 //   await fetchPolicyList();
 //   policyList.value = policyStore.PolicyInfoList;
 // });
-</script>
+</script> -->
 
-<template>
+<!-- <template>
   <div class="p-4 bg-white shadow-md rounded-lg">
     <h2 class="text-lg font-semibold mb-4">정책 필터</h2>
     <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
@@ -200,6 +200,395 @@ const applyFilters = async () => {
           닫기
         </button>
       </div>
+    </div>
+  </div>
+</template> -->
+
+<!-- <template>
+  <div class="p-4 bg-white shadow-md rounded-lg">
+    <h2 class="text-lg font-semibold mb-4">정책 필터</h2>
+    <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+      <div>
+        <label for="city" class="block text-sm font-medium text-gray-700"
+          >지역 선택</label
+        >
+        <select
+          v-model="selectedCity"
+          id="city"
+          class="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+        >
+          <option value="null">전체</option>
+          <option v-for="city in cities" :key="city" :value="city">
+            {{ city }}
+          </option>
+        </select>
+      </div>
+
+      <div>
+        <label for="district" class="block text-sm font-medium text-gray-700"
+          >지역구 선택</label
+        >
+        <input
+          v-model="district"
+          id="district"
+          type="text"
+          class="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+        />
+      </div>
+
+      <div>
+        <label for="policyType" class="block text-sm font-medium text-gray-700"
+          >정책 유형</label
+        >
+        <select
+          v-model="selectedPolicyType"
+          id="policyType"
+          class="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm"
+        >
+          <option value="null">전체</option>
+          <option v-for="type in policyTypes" :key="type" :value="type">
+            {{ type }}
+          </option>
+        </select>
+      </div>
+    </div>
+
+    <div class="mt-6">
+      <button
+        @click="applyFilters"
+        class="w-full bg-darkBlue text-white py-2 px-4 rounded-md"
+      >
+        필터 적용
+      </button>
+    </div>
+  </div>
+
+  <div class="container mx-auto p-4">
+    <p class="text-2xl font-bold mb-4 text-[32px]">정책 리스트</p>
+    <table class="w-full table-auto border-collapse border border-gray-300">
+      <thead>
+        <tr class="bg-gray-100">
+          <th class="border border-gray-300 px-4 py-2">이름</th>
+          <th class="border border-gray-300 px-4 py-2">지역</th>
+          <th class="border border-gray-300 px-4 py-2">지역구</th>
+          <th class="border border-gray-300 px-4 py-2">정책 유형</th>
+          <th class="border border-gray-300 px-4 py-2">운영 기관</th>
+          <th class="border border-gray-300 px-4 py-2">정책 시작 날짜</th>
+          <th class="border border-gray-300 px-4 py-2">정책 마감 날짜</th>
+          <th class="border border-gray-300 px-4 py-2">신청 시작 날짜</th>
+          <th class="border border-gray-300 px-4 py-2">신청 마감 날짜</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="(policy, index) in policyList"
+          :key="index"
+          class="hover:bg-gray-100 cursor-pointer"
+          @click="openPolicyDetailModal(policy.idx)"
+        >
+          <td class="border border-gray-300 px-4 py-2">{{ policy.name }}</td>
+          <td class="border border-gray-300 px-4 py-2">{{ policy.city }}</td>
+          <td class="border border-gray-300 px-4 py-2">
+            {{ policy.district }}
+          </td>
+          <td class="border border-gray-300 px-4 py-2">{{ policy.type }}</td>
+          <td class="border border-gray-300 px-4 py-2">
+            {{ policy.offerInst }}
+          </td>
+          <td class="border border-gray-300 px-4 py-2">
+            {{ policy.startDate }}
+          </td>
+          <td class="border border-gray-300 px-4 py-2">{{ policy.endDate }}</td>
+          <td class="border border-gray-300 px-4 py-2">
+            {{ policy.applyStartDate }}
+          </td>
+          <td class="border border-gray-300 px-4 py-2">
+            {{ policy.applyEndDate }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+
+    <div
+      v-if="isModalOpen"
+      class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
+    >
+      <div
+        class="bg-white rounded-lg shadow-2xl p-8 w-11/12 sm:w-3/4 md:w-1/2 lg:w-1/3 relative"
+      >
+        <p class="text-2xl font-semibold mb-4 text-[26px] text-gray-800">
+          정책 상세 정보
+        </p>
+        <div class="text-gray-700 space-y-2">
+          <p><strong>대상:</strong> {{ policyStore.PolicyDetail.subject }}</p>
+          <p><strong>조건:</strong> {{ policyStore.PolicyDetail.condition }}</p>
+          <p>
+            <strong>상세 내용:</strong> {{ policyStore.PolicyDetail.content }}
+          </p>
+          <p>
+            <strong>지원 규모:</strong> {{ policyStore.PolicyDetail.scale }}
+          </p>
+          <p><strong>문의처:</strong> {{ policyStore.PolicyDetail.enquiry }}</p>
+          <p><strong>지원 방법:</strong> {{ policyStore.PolicyDetail.way }}</p>
+          <p><strong>서류:</strong> {{ policyStore.PolicyDetail.document }}</p>
+          <p><strong>URL:</strong> {{ policyStore.PolicyDetail.url }}</p>
+        </div>
+        <button
+          class="mt-6 bg-darkBlue text-white px-6 py-2 rounded-lg w-full"
+          @click="closeModal"
+        >
+          닫기
+        </button>
+      </div>
+    </div>
+  </div>
+</template> -->
+
+<script setup>
+import { usePolicyStore } from "@/stores/policy.js";
+import { ref, computed } from "vue";
+
+const policyStore = usePolicyStore();
+const policyList = ref([]);
+const isModalOpen = ref(false);
+
+const selectedCity = ref(null);
+const district = ref(null);
+const selectedPolicyType = ref(null);
+
+const currentPage = ref(1);
+const itemsPerPage = 10;
+
+const totalPages = computed(() => {
+  return Math.ceil(policyList.value.length / itemsPerPage);
+});
+
+const paginatedPolicies = computed(() => {
+  const start = (currentPage.value - 1) * itemsPerPage;
+  const end = start + itemsPerPage;
+  return policyList.value.slice(start, end);
+});
+
+const cities = [
+  "서울",
+  "부산",
+  "대구",
+  "인천",
+  "광주",
+  "대전",
+  "울산",
+  "세종",
+  "경기",
+  "강원",
+  "충북",
+  "충남",
+  "전북",
+  "전남",
+  "경상북도",
+  "경남",
+  "제주",
+];
+const policyTypes = [
+  "일자리(창업)",
+  "일자리(취업)",
+  "건강",
+  "교육",
+  "금융",
+  "문화",
+  "복지",
+  "주거비 지원",
+  "주택공급",
+  "참여",
+];
+
+const openPolicyDetailModal = async (idx) => {
+  await policyStore.getPolicyDetail(idx);
+  isModalOpen.value = true;
+};
+
+const closeModal = () => {
+  isModalOpen.value = false;
+};
+
+const applyFilters = async () => {
+  console.log("Selected City:", selectedCity.value);
+  console.log("Selected District:", district.value);
+  console.log("Selected Policy Type:", selectedPolicyType.value);
+
+  policyStore.filterCondition.city =
+    selectedCity.value === "전체" || selectedCity.value === "null"
+      ? null
+      : selectedCity.value;
+  policyStore.filterCondition.district =
+    district.value === "" || district.value === "null" ? null : district.value;
+  policyStore.filterCondition.type =
+    selectedPolicyType.value === "전체" || selectedPolicyType.value === "null"
+      ? null
+      : selectedPolicyType.value;
+
+  await policyStore.getPolicyFilter();
+  policyList.value = policyStore.PolicyInfoList;
+  currentPage.value = 1;
+};
+
+const changePage = (page) => {
+  if (page > 0 && page <= totalPages.value) {
+    currentPage.value = page;
+  }
+};
+</script>
+
+<template>
+  <div class="p-4 bg-white shadow-md rounded-lg">
+    <h2 class="text-lg font-semibold mb-4">정책 필터</h2>
+    <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+      <div>
+        <label for="city" class="block text-sm font-medium text-gray-700"
+          >지역 선택</label
+        >
+        <select
+          v-model="selectedCity"
+          id="city"
+          class="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+        >
+          <option value="null">전체</option>
+          <option v-for="city in cities" :key="city" :value="city">
+            {{ city }}
+          </option>
+        </select>
+      </div>
+
+      <div>
+        <label for="district" class="block text-sm font-medium text-gray-700"
+          >지역구 선택</label
+        >
+        <input
+          v-model="district"
+          id="district"
+          type="text"
+          class="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+        />
+      </div>
+
+      <div>
+        <label for="policyType" class="block text-sm font-medium text-gray-700"
+          >정책 유형</label
+        >
+        <select
+          v-model="selectedPolicyType"
+          id="policyType"
+          class="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm"
+        >
+          <option value="null">전체</option>
+          <option v-for="type in policyTypes" :key="type" :value="type">
+            {{ type }}
+          </option>
+        </select>
+      </div>
+    </div>
+
+    <div class="mt-6">
+      <button
+        @click="applyFilters"
+        class="w-full bg-darkBlue text-white py-2 px-4 rounded-md"
+      >
+        필터 적용
+      </button>
+    </div>
+  </div>
+  <div class="container mx-auto p-4">
+    <p class="text-2xl font-bold mb-4 text-[32px]">정책 리스트</p>
+    <table class="w-full table-auto border-collapse border border-gray-300">
+      <thead>
+        <tr class="bg-gray-100">
+          <th class="border border-gray-300 px-4 py-2">이름</th>
+          <th class="border border-gray-300 px-4 py-2">지역</th>
+          <th class="border border-gray-300 px-4 py-2">지역구</th>
+          <th class="border border-gray-300 px-4 py-2">정책 유형</th>
+          <th class="border border-gray-300 px-4 py-2">운영 기관</th>
+          <th class="border border-gray-300 px-4 py-2">정책 시작 날짜</th>
+          <th class="border border-gray-300 px-4 py-2">정책 마감 날짜</th>
+          <th class="border border-gray-300 px-4 py-2">신청 시작 날짜</th>
+          <th class="border border-gray-300 px-4 py-2">신청 마감 날짜</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="(policy, index) in paginatedPolicies"
+          :key="index"
+          class="hover:bg-gray-100 cursor-pointer"
+          @click="openPolicyDetailModal(policy.idx)"
+        >
+          <td class="border border-gray-300 px-4 py-2">{{ policy.name }}</td>
+          <td class="border border-gray-300 px-4 py-2">{{ policy.city }}</td>
+          <td class="border border-gray-300 px-4 py-2">
+            {{ policy.district }}
+          </td>
+          <td class="border border-gray-300 px-4 py-2">{{ policy.type }}</td>
+          <td class="border border-gray-300 px-4 py-2">
+            {{ policy.offerInst }}
+          </td>
+          <td class="border border-gray-300 px-4 py-2">
+            {{ policy.startDate }}
+          </td>
+          <td class="border border-gray-300 px-4 py-2">{{ policy.endDate }}</td>
+          <td class="border border-gray-300 px-4 py-2">
+            {{ policy.applyStartDate }}
+          </td>
+          <td class="border border-gray-300 px-4 py-2">
+            {{ policy.applyEndDate }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+
+    <div class="flex justify-center mt-4">
+      <button
+        class="px-4 py-2 mx-1 bg-gray-300 rounded"
+        @click="changePage(currentPage - 1)"
+        :disabled="currentPage === 1"
+      >
+        이전
+      </button>
+      <span class="px-4 py-2">{{ currentPage }} / {{ totalPages }}</span>
+      <button
+        class="px-4 py-2 mx-1 bg-gray-300 rounded"
+        @click="changePage(currentPage + 1)"
+        :disabled="currentPage === totalPages"
+      >
+        다음
+      </button>
+    </div>
+  </div>
+
+  <div
+    v-if="isModalOpen"
+    class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
+  >
+    <div
+      class="bg-white rounded-lg shadow-2xl p-8 w-11/12 sm:w-3/4 md:w-1/2 lg:w-1/3 relative"
+    >
+      <p class="text-2xl font-semibold mb-4 text-[26px] text-gray-800">
+        정책 상세 정보
+      </p>
+      <div class="text-gray-700 space-y-2">
+        <p><strong>대상:</strong> {{ policyStore.PolicyDetail.subject }}</p>
+        <p><strong>조건:</strong> {{ policyStore.PolicyDetail.condition }}</p>
+        <p>
+          <strong>상세 내용:</strong> {{ policyStore.PolicyDetail.content }}
+        </p>
+        <p><strong>지원 규모:</strong> {{ policyStore.PolicyDetail.scale }}</p>
+        <p><strong>문의처:</strong> {{ policyStore.PolicyDetail.enquiry }}</p>
+        <p><strong>지원 방법:</strong> {{ policyStore.PolicyDetail.way }}</p>
+        <p><strong>서류:</strong> {{ policyStore.PolicyDetail.document }}</p>
+        <p><strong>URL:</strong> {{ policyStore.PolicyDetail.url }}</p>
+      </div>
+      <button
+        class="mt-6 bg-darkBlue text-white px-6 py-2 rounded-lg w-full"
+        @click="closeModal"
+      >
+        닫기
+      </button>
     </div>
   </div>
 </template>
