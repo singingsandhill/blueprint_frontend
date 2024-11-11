459d87e680435c6e23d86389664461723084dc45
kim jiwoo <annejwkim00@naver.com>
2024-11-11 15:03:32 +0900

    Fix: placeholder 추가

--- src/views/policy/PolicyRecommandPage.vue
+++ src/views/policy/PolicyRecommandPage.vue
@@ -115,6 +115,7 @@ const changePage = (page) => {
           v-model="district"
           id="district"
           type="text"
+          placeholder="지역구를 입력해주세요."
           class="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
         />
       </div>
