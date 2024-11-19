1c68cab8e9e84509320827916e52a5c25632168e
JUNG DAYE <jungyj0809@gmail.com>
2024-11-20 00:21:57 +0900

    Design: 아이디, 비밀번호 찾기 디자인 및 전처리 수정

--- src/components/layouts/NavBar.vue
+++ src/components/layouts/NavBar.vue
@@ -19,21 +19,20 @@ const handleLogout = () => {
 
 <template>
   <nav class="bg-[#002842] text-white shadow-lg rounded-xl z-50 relative">
-    <div class="container mx-auto px-6 flex justify-between items-center h-16">
+    <div class="container mx-auto px-8 flex justify-between items-center h-16">
       <!-- 로고와 메뉴 (좌측 정렬) -->
-      <div class="flex items-center space-x-8">
+      <div class="flex items-center space-x-4">
         <router-link to="/" class="flex items-center mr-8">
-          <img src="@/assets/003_1.png" alt="청사진 로고" class="w-40 h-auto" />
+          <img src="@/assets/003_1.png" alt="청사진 로고" class="w-48 h-auto" />
         </router-link>
 
-        <!-- 중간 화면 크기에서 메뉴 폰트 크기 및 간격 조정 -->
         <ul
-          class="hidden lg:flex space-x-12 md:space-x-4 text-base md:text-sm py-2"
+          class="hidden lg:flex space-x-12 md:space-x-12 md:text-base py-2"
         >
           <li class="list-none py-2">
             <router-link
               to="/policy"
-              class="text-white hover:text-gray-200 transition-colors duration-200 py-2"
+              class="text-white hover:text-gray-300 transition-colors duration-200 py-2"
             >
               정책
             </router-link>
@@ -41,7 +40,7 @@ const handleLogout = () => {
           <li class="list-none py-2">
             <router-link
               to="/subscription"
-              class="text-white hover:text-gray-200 transition-colors duration-200 py-2"
+              class="text-white hover:text-gray-300 transition-colors duration-200 py-2"
             >
               청약
             </router-link>
@@ -49,7 +48,7 @@ const handleLogout = () => {
           <li class="list-none py-2">
             <router-link
               to="/realestate"
-              class="text-white hover:text-gray-200 transition-colors duration-200 py-2"
+              class="text-white hover:text-gray-300 transition-colors duration-200 py-2"
             >
               부동산
             </router-link>
@@ -57,7 +56,7 @@ const handleLogout = () => {
           <li class="list-none py-2">
             <router-link
               to="/finance"
-              class="text-white hover:text-gray-200 transition-colors duration-200 py-2"
+              class="text-white hover:text-gray-300 transition-colors duration-200 py-2"
             >
               금융
             </router-link>
@@ -67,7 +66,7 @@ const handleLogout = () => {
 
       <!-- 우측: 계정 메뉴 -->
       <div
-        class="hidden lg:flex space-x-8 md:space-x-4 items-center text-base md:text-sm py-2"
+        class="hidden lg:flex space-x-8 md:space-x-12 items-center md:text-base py-2"
       >
         <li class="list-none py-2">
           <router-link
@@ -87,7 +86,7 @@ const handleLogout = () => {
           <router-link
             to="/"
             @click.prevent="handleLogout"
-            class="text-white hover:text-gray-200 py-4"
+            class="text-white hover:text-gray-300 py-4"
           >
             로그아웃
           </router-link>
@@ -95,13 +94,13 @@ const handleLogout = () => {
         <template v-else>
           <router-link
             to="/member/login"
-            class="text-white hover:text-gray-200 py-4"
+            class="text-white hover:text-gray-300 py-4"
           >
             로그인
           </router-link>
           <router-link
             to="/member/register"
-            class="text-white hover:text-gray-200 py-4"
+            class="text-white hover:text-gray-300 py-4"
           >
             회원가입
           </router-link>
@@ -136,19 +135,19 @@ const handleLogout = () => {
         </button>
 
         <router-link to="/" class="flex items-center" @click="toggleNavShow">
-          <img src="@/assets/003_1.png" alt="청사진 로고" class="w-40 h-auto" />
+          <img src="@/assets/003_1.png" alt="청사진 로고" class="w-56 h-auto mb-2" />
         </router-link>
 
         <router-link
           v-if="!isLogin"
           to="/member/login"
-          class="text-white hover:text-gray-200 mb-8"
+          class="text-white hover:text-gray-300 mb-8 text-base"
           @click="toggleNavShow"
         >
           <strong>로그인을 해 주세요</strong>
         </router-link>
         <span v-else class="text-white mb-8">
-          <router-link to="/member/mypage" class="text-white"
+          <router-link to="/member/mypage" class="text-white text-base"
             @click="toggleNavShow">
             <strong>{{ memberName }} 님</strong>
           </router-link>
@@ -158,18 +157,20 @@ const handleLogout = () => {
       <!-- 모바일 메뉴 항목 리스트 -->
       <ul class="w-full space-y-6 text-center">
         <li class="border-b border-gray-600 py-2">
+          <br>
           <router-link
             to="/member/myService"
-            class="text-white hover:text-gray-200 transition-colors duration-200"
+            class="text-white hover:text-gray-300 transition-colors duration-200 text-base"
             @click="toggleNavShow"
           >
             나만의 서비스
           </router-link>
         </li>
-        <li class="border-b border-gray-600 py-2">
+        <br>
+        <li class="border-b border-gray-500 py-2">
           <router-link
             to="/policy"
-            class="text-white hover:text-gray-200 transition-colors duration-200"
+            class="text-white hover:text-gray-300 transition-colors duration-200 text-base"
             @click="toggleNavShow"
           >
             정책
@@ -178,7 +179,7 @@ const handleLogout = () => {
         <li class="border-b border-gray-600 py-2">
           <router-link
             to="/subscription"
-            class="text-white hover:text-gray-200 transition-colors duration-200"
+            class="text-white hover:text-gray-300 text-base transition-colors duration-200"
             @click="toggleNavShow"
           >
             청약
@@ -187,7 +188,7 @@ const handleLogout = () => {
         <li class="border-b border-gray-600 py-2">
           <router-link
             to="/realestate"
-            class="text-white hover:text-gray-200 transition-colors duration-200"
+            class="text-white hover:text-gray-300 text-base transition-colors duration-200"
             @click="toggleNavShow"
           >
             부동산
@@ -196,7 +197,7 @@ const handleLogout = () => {
         <li class="border-b border-gray-600 py-2">
           <router-link
             to="/finance"
-            class="text-white hover:text-gray-200 transition-colors duration-200"
+            class="text-white hover:text-gray-300 text-base transition-colors duration-200"
             @click="toggleNavShow"
           >
             금융
@@ -204,13 +205,13 @@ const handleLogout = () => {
         </li>
       </ul>
 
-      <div class="flex flex-col text-center space-y-6 mt-8">
+      <div class="flex flex-col text-center space-y-6">
         <template v-if="isLogin">
           <div class="border-b border-gray-600 py-2">
             <router-link
               to="/"
               @click.prevent="handleLogout"
-              class="text-white hover:text-gray-200 transition-colors duration-200"
+              class="text-white hover:text-gray-300 text-base transition-colors duration-200"
             >
               로그아웃
             </router-link>
